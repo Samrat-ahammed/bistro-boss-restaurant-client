fc02ece090d0e852663bfdbf0adc83f85194ab6f
Samrat Ahmmed <samratahammed29@gmail.com>
2023-11-18 22:53:37 +0600

    add all user page

--- src/LayOut/DeshBoard/AllUsers.jsx
+++ src/LayOut/DeshBoard/AllUsers.jsx
@@ -1,15 +1,43 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxios from "../../CustomHooks/useAxios";
-
+import { RiDeleteBin2Fill } from "react-icons/ri";
+import { FaUser } from "react-icons/fa";
+import Swal from "sweetalert2";
 const AllUsers = () => {
   const axiosSecure = useAxios();
-  const { data: users = [] } = useQuery({
+  const { data: users = [], refetch } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await axiosSecure.get("/users");
       return res.data;
     },
   });
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/users/${id}`).then((res) => {
+          console.log(res);
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
   return (
     <div>
       <h2 className="text-violet-700 font-bold text-3xl">
@@ -24,6 +52,7 @@ const AllUsers = () => {
               <th></th>
               <th>Name</th>
               <th>Email</th>
+              <th>Role</th>
               <th>Action</th>
             </tr>
           </thead>
@@ -33,7 +62,13 @@ const AllUsers = () => {
                 <th>{index + 1}</th>
                 <td>{item.name}</td>
                 <td>{item.email}</td>
-                <td>Delete</td>
+                <td className="btn btn-primary mr-5">
+                  <FaUser className="text-white text-2xl" />
+                </td>
+                <td onClick={() => handleDelete(item._id)} className="btn">
+                  <RiDeleteBin2Fill className="text-red-800" />
+                  Delete
+                </td>
               </tr>
             ))}
           </tbody>
